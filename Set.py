0e04160476fa8d311fc00652011b565b1d1ad200
Luby Liao <lubyliao@gmail.com>
2014-10-11 15:11:15 -0700

    Replace factory method with factory function

--- Set.py
+++ Set.py
@@ -1,32 +1,10 @@
 """
-
-Assume that we would like to provide two implmentations of a Set data type.
-The motivation is that one is more efficient than the other under different situations.
-
-These two implementations use a dict and a list respectively to repesent a set.
-
-You can expect the two implementations to have very similar code. 
-For example, their implementation of union and intersection should be nearly identical.
-
-We would like to reduce code duplication as much as possible.
-
-As the code evolves, we introduced
-
-  -- factory method pattern
-
-  -- template method, such as Set.intersection(self, other) which
-     contains the logic of finding the intersection of two sets, but
-     delegates details to concrete subclasses.
-
-  -- iterator (with generator).  This allows us to write 
-
-       for x in self
-     
-     instead of "for x in self.rep", in Set.intersection
-
-  -- factory function (see next version)
+This version replaces factory method with factory function.  
 
 """
+
+def new(obj, *args, **kw):      # Factory function
+    return type(obj)(*args, **kw)
 
 def iterator(s):
     for x in s.rep:
@@ -35,9 +13,10 @@ def iterator(s):
 class Set:
 
     def intersection(self, other): # An example of a template method
-        s = self.new()          # An example of the factory method pattern, see Wikipedia
-        for x in self:          # existence of an iterator allows us to say this
-            if x in other:      # and this
+        # s = self.new()           # Used factory method in the previous version
+        s = new(self)           # Use factory function instead
+        for x in self:          
+            if x in other:      
                 s.add(x)
         return s
 
@@ -51,20 +30,11 @@ class DictSet(Set):
         for element in elements:
             rep[element] = element
 
-    def new(self):
-        return DictSet()
+    # def new(self):              # No longer needed
+    #     return DictSet()
 
-    def add(self, x):
+    def add(self, x):           
         self.rep[x] = x
-
-    # def intersection(self, other):
-    #     rep = {}
-    #     for x in self.rep:
-    #         if x in other.rep:
-    #             rep[x] = x
-    #     s = DictSet()
-    #     s.rep = rep
-    #     return s
 
 class ListSet(Set):
 
@@ -74,34 +44,13 @@ class ListSet(Set):
             if element not in rep:
                 rep.append(element)
 
-    def new(self):
-        return ListSet()
-
     def add(self, x):
         self.rep.append(x)
-    
-                
-    # def intersection(self, other):
-    #     rep = []
-    #     for x in self.rep:
-    #         if x in other.rep:
-    #             rep.append(x)
-    #     s = ListSet()
-    #     s.rep = rep
-    #     return s
-
 
 
 s = ListSet([1,2,3,1,2,3])
-t = DictSet([1,2,3,1,2,3])
-for x in s:
-    print(x)
-
-for x in t:
-    print(x)
-
-# r = ListSet([2,3,4])
-# w = s.intersection(r)
-# print(s.rep)
-# print(r.rep)
-# print(w.rep)
+r = ListSet([2,3,4])
+w = s.intersection(r)
+print(s.rep)
+print(r.rep)
+print(w.rep)
