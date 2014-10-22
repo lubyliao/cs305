ce55a4fad923b296b6283ffb4f9b958d84f9e5f6
Luby Liao <lubyliao@gmail.com>
2014-10-22 10:23:11 -0700

    Add ImmutableSet as an example of protection proxy

--- Set.py
+++ Set.py
@@ -1,5 +1,10 @@
 """
-This version replaces factory method with factory function.  
+Add ImmutableSet.  This is a nice example of a *protection proxy*.  An
+immutable set is a wrapper around an invisible ListSet instance s.  It
+protects s from alteration, and removes the services of add and
+remove.  It is a proxy for s, because for the set operations (union,
+intersection, members, ... ) that it is asked to do, it simply routes
+to s.
 
 """
 
@@ -13,7 +18,6 @@ def iterator(s):
 class Set:
 
     def intersection(self, other): # An example of a template method
-        # s = self.new()           # Used factory method in the previous version
         s = new(self)           # Use factory function instead
         for x in self:          
             if x in other:      
@@ -30,9 +34,6 @@ class DictSet(Set):
         for element in elements:
             rep[element] = element
 
-    # def new(self):              # No longer needed
-    #     return DictSet()
-
     def add(self, x):           
         self.rep[x] = x
 
@@ -43,14 +44,25 @@ class ListSet(Set):
         for element in elements:
             if element not in rep:
                 rep.append(element)
+    def members(self):
+        return tuple(self.rep)
 
     def add(self, x):
         self.rep.append(x)
 
+class ImmutableSet:
 
-s = ListSet([1,2,3,1,2,3])
-r = ListSet([2,3,4])
-w = s.intersection(r)
-print(s.rep)
-print(r.rep)
-print(w.rep)
+    def __iter__(self):
+        return iter(self.members())
+
+    def __init__(self, aList=[]):
+        s = ListSet(aList)
+        self.members = s.members
+        self.intersection = s.intersection
+
+
+i = ImmutableSet (range(5))
+j = ImmutableSet ([3,4,5,6])
+k = i.intersection(j)
+
+
