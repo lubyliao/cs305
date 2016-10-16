c245789afdeff5a30fdba900d64702fdfd003951
Luby Liao <lubyliao@gmail.com>
2016-10-15 19:10:06 -0700

    Version 2: factor intersection into superclass Set; factory method; template method

--- set/Set.py
+++ set/Set.py
@@ -1,47 +1,49 @@
 """
+Version 2: Factoring *intersection* into the superclass Set
 
-Assume that we provide two implmentation of a Set data type
-because one is more efficient in certain situations than the other.
+To do this, we use a method *add* and a factory method *new* in each concreate subclass.
 
-The 2 implementations use a dict and a list respectively to represent a set.
+*intersection* is an excellent example of a template method which
+describe the logic of an algorithm while delegating details of how
+things work, e.g., *new* and *add* to subclasses.
 
-You can expect the two implementations to have very similar code, for example, 
-in their implementation of union and intersection.
-
-We would like to reduce code duplication as much as possible.
+*s.add(x)* is an example of a down call, which calls *add* in a subclass.
+Note that *add* is not and need not be defined in *Set* 
 
 """
 
-class DictSet:
-	def __init__(self,elements=()):
-		rep = self.rep= {}
-		for element in elements:
-			rep[element] = element
-			
-	def intersection(self, other):
-		rep = {}
-		for x in self.rep:
-			if x in other.rep:
-				rep[x] = x
-		s = DictSet()
-		s.rep = rep
-		return s
+class Set:
+        def intersection(self, other):
+                s = self.new()
+                for x in self.rep:
+                        if x in other.rep:
+                                s.add(x)
+                return s
 
-class ListSet:
-	def __init__(self,elements=()):
-		rep = self.rep = []
-		for element in elements:
-			if element not in rep:
-				rep.append(element)
+class DictSet(Set):
+        def __init__(self,elements=()):
+                rep = self.rep= {}
+                for element in elements:
+                        rep[element] = element
 
-	def intersection(self, other):
-		rep = []
-		for x in self.rep:
-			if x in other.rep:
-				rep.append(x)
-		s = ListSet()
-		s.rep = rep
-		return s
+        def new(self):
+                return DictSet()
+                        
+        def add(self, x):
+                self.rep[x] = x
+
+class ListSet(Set):
+        def __init__(self,elements=()):
+                rep = self.rep = []
+                for element in elements:
+                        if element not in rep:
+                                rep.append(element)
+        def new(self):
+                return ListSet()
+
+        def add(self, x):
+                self.rep.append(x)
+
 
 s = DictSet([1,2,3,4])
 t = DictSet([3,4,5,6])
@@ -51,4 +53,4 @@ u = ListSet([1,2,3,4])
 v = ListSet([3,4,5,6])
 w = u.intersection(v)
 print(w.rep)
-	
+        
